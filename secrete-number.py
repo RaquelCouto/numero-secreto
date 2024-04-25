ce67f8eac1724a2d5c1dc450ae58101a8b169959
RaquelCouto <raquelcouto@outlook.com>
2024-04-25 13:40:41 -0300

    Adding another condition for taxes

--- secrete-number.py
+++ secrete-number.py
@@ -2,9 +2,11 @@ for i in range(0, 10):
     print(i)
 
 imposto = 0
-saldoConta = 5800
+saldoConta = 2500
 
 if (saldoConta > 3000):
     imposto = (saldoConta*(15/100))
+elif(saldoConta < 3000):
+    print("Seu saldo é inelegível para pagar imposto")
 
 print("O valor do seu imposto corresponde a: {}".format(imposto))
