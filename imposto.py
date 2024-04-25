0c7411257ec1d11dcae46ea2f3d89dd2846e4e25
RaquelCouto <raquelcouto@outlook.com>
2024-04-25 15:51:03 -0300

    Adding a new function into imposto.py and the .gitignore file

--- imposto.py
+++ imposto.py
@@ -1,3 +1,9 @@
 def imposto(salario, taxa):
     imposto = salario*(taxa/100)
     return imposto
+
+
+def deducaoImposto(salario):
+    if (salario < 3000):
+        reducaoImposto = salario*(3/100)
+        return reducaoImposto
