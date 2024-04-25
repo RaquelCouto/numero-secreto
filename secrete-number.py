9aaca1828d356dbf09aaa178638a344e752131ba
RaquelCouto <raquelcouto@outlook.com>
2024-04-25 11:11:17 -0300

    Adding a function to calculate taxes

--- secrete-number.py
+++ secrete-number.py
@@ -1,2 +1,10 @@
 for i in range(0, 10):
     print(i)
+
+imposto = 0
+saldoConta = 5800
+
+if (saldoConta > 3000):
+    imposto = (saldoConta*(15/100))
+
+print("O valor do seu imposto corresponde a: {}".format(imposto))
