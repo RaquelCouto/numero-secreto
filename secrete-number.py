a4bfa357e462c1128c01db043d2198ce8388e4b1
RaquelCouto <raquelcouto@outlook.com>
2024-04-25 13:45:45 -0300

    Revert "Adding another condition for taxes"
    This reverts commit ce67f8eac1724a2d5c1dc450ae58101a8b169959.

--- secrete-number.py
+++ secrete-number.py
@@ -2,11 +2,9 @@ for i in range(0, 10):
     print(i)
 
 imposto = 0
-saldoConta = 2500
+saldoConta = 5800
 
 if (saldoConta > 3000):
     imposto = (saldoConta*(15/100))
-elif(saldoConta < 3000):
-    print("Seu saldo é inelegível para pagar imposto")
 
 print("O valor do seu imposto corresponde a: {}".format(imposto))
